647c28faad4c2179df605b2b38a4e196ad122c20
deVamshi <dev.vamshidhar@gmail.com>
2022-09-16 18:21:06 +0530

    project details page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import AppSiderBar from "./components/app_sidebar";
 import ProjectsList from "./components/projects_list";
+import ProjectDetails from "./components/project_details";
 
 function App() {
+  
   return (
     <div>
       <div className="flex m-h-screen bg-gray-100">
         <AppSiderBar />
         <div className="flex-grow">
           <div className=" bg-wh-500 p-4">
-            <ProjectsList />
+            <ProjectDetails />
           </div>
         </div>
       </div>
